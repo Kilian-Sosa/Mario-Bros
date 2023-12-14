7c823b86ff1574e74f4660c73d8172fbd04d99a3
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-12-14 18:35:26 +0000

    feat: Stop player from jumping multiple times

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     [SerializeField] float playerSpeed = 1f;
     [SerializeField] float jumpForce = 1f;
+    [SerializeField] bool isJumping = false;
+
+    [SerializeField] float lineLength = 1f;
+    [SerializeField] float offset = 1f;
 
     void Update() {
         float direction = Input.GetAxisRaw("Horizontal");
@@ -14,7 +18,16 @@ public class PlayerController : MonoBehaviour {
         if (direction == -1)
             GetComponent<SpriteRenderer>().flipX = true; 
         
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && !isJumping)
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - offset);
+        Vector2 target = new Vector2(transform.position.x, transform.position.y - offset - lineLength);
+        Debug.DrawLine(origin, target, Color.black);
+
+        RaycastHit2D raycast = Physics2D.Raycast(origin, Vector2.down, lineLength);
+
+        if (raycast.collider == null) isJumping = true;
+        else isJumping = false;
     }
 }
